2672dd742e7e127af8a044e962f517b4d8b97104
GhostX <egamovkhurshed29@gmail.com>
2020-06-18 15:19:31 +0500

    Cascade deleting with categories

--- Areas/Admin/Controllers/CategoriesController.cs
+++ Areas/Admin/Controllers/CategoriesController.cs
@@ -37,6 +37,20 @@ namespace ChaVo.Areas.Admin.Controllers
         public async Task<IActionResult> Delete(int id)
         {
             var model = await _context.Categories.FirstAsync(c=>c.Id == id);
+            var QuestionLi = await _context.Questions.Include(q=>q.Category).Where(q=> q.Category.Id == model.Id).ToListAsync();
+            foreach (var x in QuestionLi)
+            {
+                var QuestionModel = await _context.Questions.FirstAsync(q=>q.Id == x.Id);
+                var AnswerLi = await _context.Answers.Include(a=>a.Question).Where(a=>a.Question.Id == x.Id).ToListAsync();
+                foreach (var answer in AnswerLi)
+                {
+                    var AnswerModel = await _context.Answers.FirstAsync(a=>a.Id == answer.Id);
+                    _context.Answers.Remove(AnswerModel);
+                    await _context.SaveChangesAsync();
+                }
+                _context.Questions.Remove(QuestionModel);
+                await _context.SaveChangesAsync();
+            }
             _context.Categories.Remove(model);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
